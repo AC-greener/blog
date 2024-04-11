a9de0c74cd262ca8a99f5e1fcb069ed1f3fab636
AC-greener <15929774304@163.com>
2024-04-11 16:47:42 +0800

    feat：add webAnalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 
 export default defineConfig({
     site: 'https://www.yourwebsite.com',
-
+    output: 'server',
+    adapter: vercel({
+      webAnalytics: { enabled: true }
+    }),
 });
