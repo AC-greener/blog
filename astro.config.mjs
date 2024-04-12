1ad1d2bb6854e3ca58bb7c34d1d3bd2e0c42192b
AC-greener <15929774304@163.com>
2024-04-12 17:20:06 +0800

    feat: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://www.yourwebsite.com',
-    output: 'server',
-    adapter: vercel({
-      webAnalytics: { enabled: true }
-    }),
+  site: 'https://www.yourwebsite.com',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  integrations: [react()]
 });
