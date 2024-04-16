1d9290385e769b1cc7c1a38f12e74a7dd31416f3
AC-greener <15929774304@163.com>
2024-04-16 12:46:38 +0800

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.yourwebsite.com',
+  site: 'https://www.zhutongtong.cn',
   output: 'server',
   adapter: vercel({
     webAnalytics: {
