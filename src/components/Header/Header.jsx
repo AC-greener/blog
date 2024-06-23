a26c1d1d3deefb5770f792b5ea7bcad3fd399c75
zhutongtong <15929774304@163.com>
2024-06-23 17:34:23 +0800

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import "./Header.less"
 export default function Header() {
   return (
-    <header>
+    <header className="h-[72px] leading-[72px]">
       <nav>
-        <div class="nav-links">
-          <a href="/">Home</a>
+        <div>
+          <a className="text-lg text-neutral-600/80 hover:text-neutral-600" href="/">Home</a>
           {/* <!-- <a href="/about">关于</a> --> */}
           {/* <a href="/blog">blog</a> */}
           {/* <a href="/tags/astro">Tag</a> */}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,21 @@
 // 将日期字符串转换为 "2022/09/19" 格式
-// function formatDate(dateString: string) {
-//   const year = dateString.substring(0, 4);
-//   const month = dateString.substring(4, 6);
-//   const day = dateString.substring(6, 8);
-
-//   return `${year}/${month}/${day}`;
-// }
-// 格式化日期为 'October 11, 2023' 的格式
 function formatDate(dateString) {
+  if (!dateString) return '';
   const year = dateString.substring(0, 4);
   const month = dateString.substring(4, 6);
   const day = dateString.substring(6, 8);
-  const dateObj = new Date(year, month - 1, day);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return dateObj.toLocaleDateString('en-US', options);
+
+  return `${year}/${month}/${day}`;
 }
+// 格式化日期为 'October 11, 2023' 的格式
+// function formatDate(dateString) {
+//   const year = dateString.substring(0, 4);
+//   const month = dateString.substring(4, 6);
+//   const day = dateString.substring(6, 8);
+//   const dateObj = new Date(year, month - 1, day);
+//   const options = { year: 'numeric', month: 'long', day: 'numeric' };
+//   return dateObj.toLocaleDateString('en-US', options);
+// }
 export {
   formatDate
 }
