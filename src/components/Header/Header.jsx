b5f0657619ca2eacc67fde824fe8230e2a7a88e7
zhutongtong <15929774304@163.com>
2024-06-24 23:00:42 +0800

    add about

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ export default function Header() {
     <header className="h-[72px] leading-[72px]">
       <nav>
         <div>
-          <a className="text-lg text-neutral-600/80 hover:text-neutral-600" href="/">Home</a>
-          {/* <!-- <a href="/about">关于</a> --> */}
+          <a className="text-lg text-neutral-600/80 hover:text-neutral-600 mr-8" href="/">Home</a>
+          <a className="text-lg text-neutral-600/80 hover:text-neutral-600 mr-8"  href="/about">About</a>
           {/* <a href="/blog">blog</a> */}
           {/* <a href="/tags/astro">Tag</a> */}
         </div>
---
+++ src/pages/about/index.md
@@ -0,0 +1,12 @@
+---
+layout: ../../layouts/About.astro
+---
+
+我是一个程序员，专注于前端开发，最近在学习区块链和Web3
+
+有两只非常可爱的猫，想要开着房车去环游世界，先环游中国吧:)
+
+![https://static.zhutongtong.cn/uPic/2024062422563617192409961719240996671gL9xjh.jpg](https://static.zhutongtong.cn/uPic/2024062422563617192409961719240996671gL9xjh.jpg)
+
+![image-20240624225813121](https://static.zhutongtong.cn/uPic/2024062422581717192410971719241097751zLY4pK.png)
+
